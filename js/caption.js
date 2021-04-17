cf3c70f39527ab7e291a1f0b2211fb0eee7ad7df
Christian Paneda <ccpaneda@umich.edu>
2021-04-17 19:54:24 -0400

    made changes

---
+++ js/caption.js
@@ -0,0 +1,14 @@
+
+
+const createCaptionButton = document.getElementById("create-caption-button");
+const caption = document.getElementById("new-caption")
+
+
+createCaptionButton.addEventListener("click", () =>{
+    let emoji = document.getElementById("emoji").value;
+    let prettyAdjective = document.getElementById("adjective1").value;
+    let talentedAdjective = document.getElementById("adjective2").value;
+    let keyboardSmash = document.getElementById("keyboard-smash").value; 
+
+    caption.textContent = emoji + " Kaitlin! " + keyboardSmash + " You're so " + prettyAdjective + "!!! " + "No one is as " + talentedAdjective + " you!! " + emoji;
+})
