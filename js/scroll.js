f51ccdbe5239577a0426015cdf50fb478575b6cc
Christian Paneda <ccpaneda@umich.edu>
2021-04-17 14:27:54 -0400

    make changes

--- js/scroll.js
+++ js/scroll.js
@@ -1,8 +1,5 @@
 const pixelsTag = document.querySelector("h3.pixel")
 const bodyTag = document.querySelector("body")
-const sectionThresholds = document.querySelectorAll("section")
-const aboutTag = document.querySelector("h2.about") 
-const headerTag = document.querySelector("header")
 
 
 // when scrolling the page, update the pixels tag 
@@ -16,30 +13,8 @@ document.addEventListener("scroll", () => {
     pixelsTag.innerHTML = "Progress: "  + Math.round(percentage) + "%";
 });
 
-// offset check for each section
+// scroll effects 
 
-document.addEventListener("scroll", () => {
-    const pixels = window.pageYOffset
 
-    sectionThresholds.forEach(section => {
-        if (section.offsetTop <= pixels){
-            aboutTag.innerHTML = section.getAttribute("data-kaitlin")
-            
 
-            if (section.classList.contains("data-is-dark")){
-                headerTag.classList.add("white")
-            }
-            else{
-                headerTag.classList.remove("white")
-            }
-        }
 
-      
-    })
-
-})
-
-// user scrolls [done]
-// check if it has passed the threshold
-// if it has passed the threshold, then change the title
-// if it has passed the threshold, then change the color
